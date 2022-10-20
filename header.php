01aee8d0d35161e27e8cbd6d4b1dfeeb3e011bf6
Vishal Kumar <vishal393.vk@gmail.com>
2022-10-20 13:31:18 +0530

    Beta Testing

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vishal - Portfolio</title>
+    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" media="screen and (max-with)" href="./CSS/mobile.css">
+
+</head>
+<body>
+    <div class="header">
+        <div class="navbar">
+        <h2>Vishal Kumar</h2>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        <p> <?php echo Date("d-M-Y"); ?></p>
+    </div>
+    </div>
